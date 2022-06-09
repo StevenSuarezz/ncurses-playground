f886c9d04efdd0c335ba1b6c75dbcca2644e0d54
Steven Suarez <steven@Stevens-MacBook-Air.local>
2022-06-08 23:24:38 -0400

    Moving the cursor

--- testncurses.cpp
+++ testncurses.cpp
@@ -5,10 +5,29 @@ int main()
 	// Initializes screen, sets up memory and clears screen
 	initscr();
 
+	int x {10};
+	int y {10};
+
+	// Move the cursor to the specified location (y, x)
+	move(y, x);
+
 	printw("Hello World!");
 
 	// Refreshes screen to match memory
 	refresh();
+	
+	// Store character code in int var
+	int charCode = getch();
+
+	// Clear the screen
+	clear();
+
+	// move and print in one command
+	//mvprintw(0,0, "%d", charCode);
+
+	// More verbose to move and print in separate lines
+	move(0, 0);
+	printw("%d", charCode);
 
 	// Waits for user input, returns int value of key
 	getch();
